b69d7be032aca9d344f8d1b3ff99651a9389538d
Egor krivov <egor.krivov@frtk.ru>
2017-12-20 20:34:11 +0300

    chain verification works

--- crypto_utils.py
+++ crypto_utils.py
@@ -1,4 +1,5 @@
 import hashlib
+from collections import defaultdict
 
 from money_source import money_source
 from log import logging
@@ -19,14 +20,15 @@ def model_transactions(transactions, starting_purse: dict, signatures) -> ([dict
     Checks for negative balance and wrong signatures.
     Returns all valid transactions and final purse.
     """
-    purse = starting_purse.copy()
+    purse = defaultdict(lambda: 0)
+    purse.update(starting_purse)
     verified_transactions = []
     for transaction in transactions:
         logging.debug('Processing transaction ' + str(transaction))
         sender, recipient, amount = transaction['sender'], transaction['recipient'], transaction['amount']
         if not verify_transaction_signature(transaction, signatures[sender]):
             logging.debug('Signature was wrong')
-        elif sender not in purse or recipient not in purse:
+        elif sender not in signatures or recipient not in signatures:
             logging.debug('Unknown parties')
         elif amount <= 0 or sender == recipient or (sender != money_source and purse[sender] - amount < 0):
             logging.debug('Impossible operation')
@@ -36,12 +38,35 @@ def model_transactions(transactions, starting_purse: dict, signatures) -> ([dict
                 purse[sender] -= amount
             purse[recipient] += amount
             verified_transactions.append(transaction)
-    return transactions, purse
+    return verified_transactions, purse
 
 
 def verify_block_hash(block):
     block_hash = compute_block_hash(block)
     return block_hash == block['hash'] and verify_block_hash_difficulty(block_hash)
+
+
+def verify_chain(chain, purse: dict, signatures: dict):
+    """Determine if a given blockchain is valid"""
+
+    verify_block_hash(chain[0])
+    logging.debug('Verifying hash')
+    for i, block in enumerate(chain):
+        if i > 0:
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != chain[i - 1]['hash']:
+                return False
+
+        # Check that the Proof of Work is correct
+        if not verify_block_hash(block):
+            return False
+
+        # Check that transactions were correct
+        valid_transactions, purse = model_transactions(block['transactions'], purse, signatures)
+        if len(block['transactions']) != len(valid_transactions):
+            return False
+
+    return True
 
 
 # Crypto functions for blocks
@@ -59,6 +84,7 @@ def compute_transaction_hash(transaction):
     transaction_header = '|'.join([transaction['sender'], transaction['recipient'], str(transaction['amount']),
                                    transaction['signature']])
     return hashlib.sha256(transaction_header.encode('utf-8')).hexdigest()
+
 
 # Signing
 def verify_transaction_signature(transaction, signature):
--- test_blockchain.py
+++ test_blockchain.py
@@ -1,6 +1,7 @@
 import unittest
 
 from blockchain import Blockchain
+from crypto_utils import verify_chain
 
 
 class TestBlockchain(unittest.TestCase):
@@ -19,4 +20,10 @@ class TestBlockchain(unittest.TestCase):
         self.assertEqual(blockchain.purse['bob'], 150)
         self.assertEqual(blockchain.purse[blockchain.node_identifier], 3)
 
-        #blockchain.ver
+        self.assertTrue(verify_chain(blockchain.chain, {blockchain.node_identifier: 0, '0': 0},
+                                     {blockchain.node_identifier: '0', '0': '0', 'alice': '1234', 'bob': '1243'}))
+
+        blockchain.chain[1]['transactions'][0]['amount'] = 1
+
+        self.assertFalse(verify_chain(blockchain.chain, {blockchain.node_identifier: 0, '0': 0},
+                                      {blockchain.node_identifier: '0', '0': '0', 'alice': '1234', 'bob': '1243'}))
